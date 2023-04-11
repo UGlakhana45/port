c348b9f59e0fc15bf5fb4da28c7c32732c447a32
UGlakhana45 <udayl.itpath@gmail.com>
2023-04-11 17:33:45 +0530

    portfolio done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./page/Home";
+import BlogArchive from "./components/BlogArchive";
 
-const App = () => {
+import { Routes, Route } from "react-router-dom";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<BlogArchive />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
